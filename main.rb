5fe5a6964670d01c9227163e68985ead4a126f5f
Matt Reisen <matthewdreisen@hotmail.com>
2017-08-03 14:00:07 -0500

    Added fill, working sum

--- main.rb
+++ main.rb
@@ -11,11 +11,19 @@ end
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
 	@people[row["Account"].chomp.to_sym][row["Category"].chomp.to_sym] = []
 end
-binding.pry
 
 CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
 	account = row["Account"].chomp.to_sym
 	category = row["Category"].chomp.to_sym
-	outflow = row["Outflow"]
-	inflow = row["Inflow"]
+	outflow = - row["Outflow"].gsub("$", "").gsub(",","").to_f
+	inflow = row["Inflow"].gsub("$","").gsub(",","").to_f
+@people[account][category] << outflow + inflow
+end
+
+amount = 0
+@people[account][category].each {|amount| += amount}
+
+binding.pry
+
+puts "nil"
 
