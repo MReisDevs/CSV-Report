a826c241120d5d7d4c8107dcadd865ebd6925f33
Matt Reisen <matthewdreisen@hotmail.com>
2017-08-07 15:55:05 -0500

    More funtionality

--- main.rb
+++ main.rb
@@ -33,14 +33,39 @@ def TotalBal(account)
 	total.round(2)
 end
 
+
+
+def nextprompt(account)
+	puts "What would you like to view next?\nFor Account Overview type 'Overview'\nFor Category Details type 'Category'\n To Exit type 'Exit' "
+	choice = gets.chomp.capitalize
+	if choice == "Overview"
+		puts "This is a placeholder but your total balance is #{TotalBal(account)}"
+	elsif choice == "Category"
+		puts "Some sort of magic category info here"
+	elsif choice == "Exit"
+		puts "Goodbye! Have a great day!"
+	else
+		puts "Mistakes were made, please try again"
+		puts nextprompt(account)
+	end
+end
+
+
+puts "What is your name on account?"
 account = gets.chomp.capitalize.to_sym
-if account == :Priya || account == :Sonia
-	puts "Welcome #{account}!\nHere is your overall balance $ #{TotalBal(account)}"
+if @people.include?(account)
+	puts "Welcome #{account}!\nHere is your overall balance: $ #{TotalBal(account)}\n===============================================\n"
+	puts nextprompt(account)
+
 else
 	puts "Please try another account"
 end
 
-binding.pry
+binding.pry 
+
+#
+#
+
 
 puts "Pry Placeholder \n Abandon all hope \n ye who debug here!"
 
